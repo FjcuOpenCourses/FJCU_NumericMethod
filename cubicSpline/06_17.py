65a5ffb673b0ae40c5eb312fb94356d80018db61
DanielDu <danghoangnhan.1@gmail.com>
2021-06-21 04:19:12 +0800

    fix the cubicSlpine class to be more reusueable

--- cubicSpline/06_17.py
+++ cubicSpline/06_17.py
@@ -10,71 +10,13 @@ def main():
     myCubicSpline = cubicSpline()
     myCubicSpline.Setup(os.path.dirname(os.path.realpath(__file__)) + "\\datasheet.in")
 
-    for i in range(myCubicSpline.n - 1):
-        myCubicSpline.H[i] = myCubicSpline.dataX[i + 1] - myCubicSpline.dataX[i]
 
 
-    if myCubicSpline.case == 1:
-        myCubicSpline.A[0][0] = 2 * (myCubicSpline.H[0] + myCubicSpline.H[1])
-        myCubicSpline.A[0][1] = myCubicSpline.H[1]
-        myCubicSpline.A[myCubicSpline.n - 3][myCubicSpline.n - 4] = myCubicSpline.H[myCubicSpline.n - 3]
-        myCubicSpline.A[myCubicSpline.n - 3][myCubicSpline.n - 3] = 2 * (
-                    myCubicSpline.H[myCubicSpline.n - 3] + myCubicSpline.H[myCubicSpline.n - 2])
+    myCubicSpline.generatingAMaxtrixValue()
 
-    elif myCubicSpline.case == 2:
-        myCubicSpline.A[0][0] = 3 * myCubicSpline.H[0] + 2 * myCubicSpline.H[1]
-        myCubicSpline.A[0][1] = myCubicSpline.H[1]
-        myCubicSpline.A[myCubicSpline.n - 3][myCubicSpline.n - 4] = myCubicSpline.H[myCubicSpline.n - 3]
-        myCubicSpline.A[myCubicSpline.n - 3][myCubicSpline.n - 3] = 2 * myCubicSpline.H[myCubicSpline.n - 3] + 3 * \
-                                                                    myCubicSpline.H[myCubicSpline.n - 2]
-    else:
-        myCubicSpline.A[0][0] = (myCubicSpline.H[0] + myCubicSpline.H[1]) * (
-                    myCubicSpline.H[0] + 2 * myCubicSpline.H[1]) / myCubicSpline.H[1]
-        myCubicSpline.A[0][1] = (myCubicSpline.H[1] ** 2 - myCubicSpline.H[0] ** 2) / myCubicSpline.H[1]
-        myCubicSpline.A[myCubicSpline.n - 3][myCubicSpline.n - 4] = (myCubicSpline.H[myCubicSpline.n - 3] ** 2 -
-                                                                     myCubicSpline.H[myCubicSpline.n - 2] ** 2) / \
-                                                                    myCubicSpline.H[myCubicSpline.n - 3]
-        myCubicSpline.A[myCubicSpline.n - 3][myCubicSpline.n - 3] = (myCubicSpline.H[myCubicSpline.n - 2] +
-                                                                     myCubicSpline.H[myCubicSpline.n - 3]) * (
-                                                                                myCubicSpline.H[
-                                                                                    myCubicSpline.n - 2] + 2 *
-                                                                                myCubicSpline.H[myCubicSpline.n - 3]) / \
-                                                                    myCubicSpline.H[myCubicSpline.n - 3]
+    myCubicSpline.generatingBMaxtrixValue()
 
-    for i in range(1, myCubicSpline.n - 3):
-        myCubicSpline.A[i][i - 1] = myCubicSpline.H[i]
-        myCubicSpline.A[i][i] = 2 * (myCubicSpline.H[i] + myCubicSpline.H[i + 1])
-        myCubicSpline.A[i][i + 1] = myCubicSpline.H[i + 1]
-
-    for i in range(myCubicSpline.n - 2):
-        u = (myCubicSpline.dataY[i + 2] - myCubicSpline.dataY[i + 1]) / myCubicSpline.H[i + 1]
-        d = (myCubicSpline.dataY[i + 1] - myCubicSpline.dataY[i]) / myCubicSpline.H[i]
-        myCubicSpline.B[i] = 6 * (u - d)
-
-    S = np.linalg.solve(myCubicSpline.A, myCubicSpline.B)
-    print("Origin S = ")
-    print(S)
-
-    if myCubicSpline.case == 1:
-        S = np.insert(S, 0, 0)
-        S = np.append(S, 0)
-
-    elif myCubicSpline.case == 2:
-        S = np.insert(S, 0, S[0])
-        S = np.append(S, S[-1])
-    else:
-        t1 = ((myCubicSpline.H[0] + myCubicSpline.H[1]) * S[0] - myCubicSpline.H[0] * S[1]) / myCubicSpline.H[1]
-        t2 = ((myCubicSpline.H[myCubicSpline.n - 3] + myCubicSpline.H[myCubicSpline.n - 2]) * myCubicSpline.S[-1] -
-              myCubicSpline.H[myCubicSpline.n - 2] * S[-2]) / myCubicSpline.H[myCubicSpline.n - 3]
-        S = np.insert(S, 0, t1)
-        S = np.append(S, t2)
-
-    for i in range(myCubicSpline.n - 1):
-        myCubicSpline.ai[i] = (S[i + 1] - S[i]) / (6 * myCubicSpline.H[i])
-        myCubicSpline.bi[i] = S[i] / 2
-        myCubicSpline.ci[i] = (float(myCubicSpline.dataY[i + 1]) - float(myCubicSpline.dataY[i])) / myCubicSpline.H[
-            i] - (2 * myCubicSpline.H[i] * S[i] + myCubicSpline.H[i] * S[i + 1]) / 6
-        myCubicSpline.di[i] = float(myCubicSpline.dataY[i])
+    myCubicSpline.fixTheSMatrix()
 
     print(myCubicSpline.ai[7], myCubicSpline.bi[7], myCubicSpline.ci[7], myCubicSpline.di[7])
 
@@ -84,7 +26,6 @@ def main():
     for i in range(myCubicSpline.n - 1):
         d = np.linspace(myCubicSpline.dataX[i], myCubicSpline.dataX[i + 1], 100)
         plt.plot(d, myCubicSpline.Poly(d, i), '--')
-
     plt.show()
 
 
--- cubicSpline/subicSpline.py
+++ cubicSpline/subicSpline.py
@@ -13,7 +13,7 @@ class cubicSpline:
         self.A = np.zeros((self.n - 2, self.n - 2))
         self.B = np.zeros(self.n - 2)
         self.H = np.zeros(self.n + 1)
-
+        self.S: float = []
         self.dataX = []
         self.dataY = []
 
@@ -23,13 +23,81 @@ class cubicSpline:
         self.di = np.zeros(self.n - 1)
 
     def Setup(self, dir):
+        # read x and y to an array
         with open(dir) as openfileobject:
             for line in openfileobject:
                 valueInput = line.replace('\n', '').split()
                 self.dataX.append(float(valueInput[0]))
                 self.dataY.append(float(valueInput[1]))
 
+        self.generateH()
+        self.generatingAMaxtrixValue()
+        self.generatingBMaxtrixValue()
+        self.S = np.linalg.solve(self.A, self.B)
+        print("Origin S = ")
+        print(self.S)
+        self.fixTheSMatrix()
+
+        for i in range(self.n - 1):
+            self.ai[i] = (self.S[i + 1] - self.S[i]) / (6 * self.H[i])
+            self.bi[i] = self.S[i] / 2
+            self.ci[i] = (float(self.dataY[i + 1]) - float(self.dataY[i])) / self.H[
+                i] - (2 * self.H[i] * self.S[i] + self.H[i] * self.S[i + 1]) / 6
+            self.di[i] = float(self.dataY[i])
+
     # P3(x)i = ai(x-xi)^3 + bi(x-xi)^2 + ci(x-xi) + di
     def Poly(self, x: float, i: int):
         return self.ai[i] * (x - self.dataX[i]) ** 3 + self.bi[i] * (x - self.dataX[i]) ** 2 + self.ci[i] * (
-                    x - self.dataX[i]) + self.di[i]
+                x - self.dataX[i]) + self.di[i]
+
+    def generatingAMaxtrixValue(self):
+
+        if self.case == 1:
+            self.A[0][0] = 2 * (self.H[0] + self.H[1])
+            self.A[0][1] = self.H[1]
+            self.A[self.n - 3][self.n - 4] = self.H[self.n - 3]
+            self.A[self.n - 3][self.n - 3] = 2 * (self.H[self.n - 3] + self.H[self.n - 2])
+
+        elif self.case == 2:
+            self.A[0][0] = 3 * self.H[0] + 2 * self.H[1]
+            self.A[0][1] = self.H[1]
+            self.A[self.n - 3][self.n - 4] = self.H[self.n - 3]
+            self.A[self.n - 3][self.n - 3] = 2 * self.H[self.n - 3] + 3 * self.H[self.n - 2]
+        else:
+            self.A[0][0] = (self.H[0] + self.H[1]) * (
+                    self.H[0] + 2 * self.H[1]) / self.H[1]
+            self.A[0][1] = (self.H[1] ** 2 - self.H[0] ** 2) / self.H[1]
+            self.A[self.n - 3][self.n - 4] = (self.H[self.n - 3] ** 2 - self.H[self.n - 2] ** 2) / \
+                                             self.H[self.n - 3]
+            self.A[self.n - 3][self.n - 3] = (self.H[self.n - 2] + self.H[self.n - 3]) * (
+                    self.H[self.n - 2] + 2 * self.H[self.n - 3]) / self.H[self.n - 3]
+
+        for i in range(1, self.n - 3):
+            self.A[i][i - 1] = self.H[i]
+            self.A[i][i] = 2 * (self.H[i] + self.H[i + 1])
+            self.A[i][i + 1] = self.H[i + 1]
+
+    def generatingBMaxtrixValue(self):
+        for i in range(self.n - 2):
+            u = (self.dataY[i + 2] - self.dataY[i + 1]) / self.H[i + 1]
+            d = (self.dataY[i + 1] - self.dataY[i]) / self.H[i]
+            self.B[i] = 6 * (u - d)
+
+    def fixTheSMatrix(self):
+        if self.case == 1:
+            self.S = np.insert(self.S, 0, 0)
+            self.S = np.append(self.S, 0)
+
+        elif self.case == 2:
+            self.S = np.insert(self.S, 0, self.S[0])
+            self.S = np.append(self.S, self.S[-1])
+        else:
+            t1 = ((self.H[0] + self.H[1]) * self.S[0] - self.H[0] * self.S[1]) / self.H[1]
+            t2 = ((self.H[self.n - 3] + self.H[self.n - 2]) * self.S[-1] -
+                  self.H[self.n - 2] * self.S[-2]) / self.H[self.n - 3]
+            self.S = np.insert(self.S, 0, t1)
+            self.S = np.append(self.S, t2)
+
+    def generateH(self):
+        for i in range(self.n - 1):
+            self.H[i] = self.dataX[i + 1] - self.dataX[i]
